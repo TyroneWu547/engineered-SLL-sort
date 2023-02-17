a1f94b45187c39c0280735a59a8cb34d5edc299e
tkwu <tkwu@ncsu.edu>
2023-02-17 04:52:44 -0500

    added simple main with generic sort impl header

---
+++ include/Sort.h
@@ -0,0 +1,6 @@
+#ifndef SORT_H
+#define SORT_H
+
+int sort(int head);
+
+#endif // SORT_H
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../include/Sort.h"
+#include "../include/Time.h"
+
+int main() {
+    std::cout << sort(10) << std::endl;
+    return 0;
+}
