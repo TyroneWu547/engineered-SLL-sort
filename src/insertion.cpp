c29cbd9a972c363e51213fe286cbd1dd173b8512
tkwu <tkwu@ncsu.edu>
2023-02-17 04:53:14 -0500

    added basic sort files for testing

---
+++ src/insertion.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../include/Sort.h"
+
+int sort(int head) {
+    std::cout << "insertion" << std::endl;
+    return head;
+}
---
+++ src/merge.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../include/Sort.h"
+
+int sort(int head) {
+    std::cout << "merge" << std::endl;
+    return head;
+}
