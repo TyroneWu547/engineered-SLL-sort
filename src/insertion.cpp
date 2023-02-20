d9b2a316cfbcb2d899957760d56c63d48e8b30b8
troync <32421060+troync@users.noreply.github.com>
2023-02-20 08:09:45 -0500

    It works on larger sets, but not the largest

--- src/insertion.cpp
+++ src/insertion.cpp
@@ -2,43 +2,40 @@
 #include "../include/Sort.h"
 #include "../include/LinkedList.h"
 
+using namespace std;
 Node* tail;
+Node* spot;
+Node* nextSpot;
 
-//This method inserts a node into the sorted portion of the list
+//This recursive method inserts a node into the sorted portion of the list
 Node* insert(Node* h, Node* spot){
-    //Case where we have found where we want to insert the node and we are not at the end of the list
     if(spot->data <= h->data){
         spot->next = h;
         return spot;
-    //Case where the node needs to go at the end of the list
-    } else if (h->next == nullptr){
+    } else if (h->next == nullptr) {
         spot->next = nullptr;
         h->next = spot;
         tail = spot;
         return h;
-    //Case where we need to keep iterating to find the insert point
-    } else {
-        h->next = insert(h->next, spot);
-        return h;
     }
+    h->next = insert(h->next, spot);
+    return h;
 };
 
 //This method iterates through the unsorted portion of the list
-Node* insertionRec(Node* h, Node* spot) {
-    Node* temp = spot->next;
-    h = insert(h, spot);
-    //Checks to see if we have run out of unsorted list nodes
-    if (temp != nullptr){
-        return insertionRec(h, temp);
-    } else {
-        return h;
+Node* insertionRec(Node* h) {
+    spot = h->next;
+    h->next = nullptr;
+    while (spot != nullptr) {
+        nextSpot = spot->next;
+        h = insert(h, spot);
+        spot = nextSpot;
     }
+    return h;
 };
 
 //Runs through the method to run insertion sort on the list and then allocates the head and tail afterwards
 void sort(LinkedList* list) {
-    Node* spot = list->head->next;
-    list->head->next = nullptr;
-    list->head = insertionRec(list->head, spot);
+    list->head = insertionRec(list->head);
     list->tail = tail;
 };
