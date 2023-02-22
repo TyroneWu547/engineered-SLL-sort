5221926856e58254144081b90efb18d7fab7f1ab
troync <32421060+troync@users.noreply.github.com>
2023-02-21 22:56:19 -0500

    Quick sort appears to be working

--- src/quick.cpp
+++ src/quick.cpp
@@ -2,38 +2,69 @@
 #include "../include/Sort.h"
 #include "../include/LinkedList.h"
 
-Node* getPivot(Node* m, Node* t, Node* h){
-    if(t->next == nullptr || t->next->next == nullptr){
-        if(m->data < t->data && t->data < h->data)
-            return t;
-        else if(m->data < h->data && h->data < t->data)
-            return h;
-        else
-            return m;
-    } 
-    return getPivot(m->next, t->next->next, h);
+using namespace std;
+
+Node* getPivot(Node* h) {
+    Node* m = h;
+    Node* t = h;
+
+    while (t->next != nullptr) {
+        t = t->next;
+        if (t->next != nullptr) {
+            m = m->next;
+            t = t->next;
+        }
+    }
+
+    if(m->data < t->data && t->data < h->data)
+        return t;
+    else if(m->data < h->data && h->data < t->data)
+        return h;
+    else
+        return m;
 }
 
-Node* getPivot(Node* h){
-    if(h->next == nullptr || h->next->next == nullptr)
-        return h;
-    return getPivot(h->next, h->next->next, h);
-}
+// Node* getPivot(Node* m, Node* t, Node* h){
+//     if(t->next == nullptr || t->next->next == nullptr){
+//         if(m->data < t->data && t->data < h->data)
+//             return t;
+//         else if(m->data < h->data && h->data < t->data)
+//             return h;
+//         else
+//             return m;
+//     } 
+//     return getPivot(m->next, t->next->next, h);
+// }
+
+// Node* getPivot(Node* h){
+//     if(h->next == nullptr || h->next->next == nullptr)
+//         return h;
+//     return getPivot(h->next, h->next->next, h);
+// }
 
 Node* findEnd(Node* h) {
-    if (h->next != nullptr)
-        return findEnd(h->next);
-    return h; 
+    Node* t = h;
+    while (t->next != nullptr)
+        t= t->next;
+    return t; 
 }
 
-Node* combine(Node* l, Node* m, Node* r){
-    findEnd(m)->next = r;
+// Node* combine(Node* l, Node* m, Node* r){
+//     findEnd(m)->next = r;
+//     if (l != nullptr){
+//         findEnd(l)->next = m;
+//         return l;
+//     }
+//     return m;
+
+// }
+
+Node* combine(Node* l, Node* r){
     if (l != nullptr){
-        findEnd(l)->next = m;
+        findEnd(l)->next = r;
         return l;
     }
-    return m;
-    
+    return r;
 }
 
 Node* quick(Node* h, Node* p){
@@ -46,10 +77,10 @@ Node* quick(Node* h, Node* p){
         Node* hold;
         do{
             hold = t->next;
-            if(t->data < p->data){
+            if(t < p){
                 t->next = l;
                 l = t;
-            } else if(t->data > p->data) {
+            } else if(t > p) {
                 t->next = r;
                 r = t;
             } else {
@@ -60,15 +91,18 @@ Node* quick(Node* h, Node* p){
         } while (t != nullptr);
 
         if(l != nullptr)
-            l = quick(l, getPivot(l));    
+            l = quick(l, getPivot(l));   
+
+        l = combine(l, m); 
+
         if(r != nullptr)
             r = quick(r, getPivot(r));
 
-        return combine(l, m, r);
+        return combine(l, r);
     }
     return h;
 }
 
 void sort(LinkedList* list) {
-    quick(list->head, getPivot(list->head));
+    list->head = quick(list->head, getPivot(list->head));
 }
