7a3477c296ac5a3d1fc9f818923d7878472cbfc1
tkwu <tkwu@ncsu.edu>
2023-02-21 22:01:32 -0500

    added comment doc

--- src/merge.cpp
+++ src/merge.cpp
@@ -1,7 +1,9 @@
 /**
  * @file merge.cpp
- * @author your name (you@domain.com)
- * @brief 
+ * @author Andrew Sauerbrei
+ *         AJ Bulthuis
+ *         Tyrone Wu
+ * @brief The merge sort implementation. 
  * 
  */
 
@@ -9,34 +11,56 @@
 #include "../include/Sort.h"
 #include "../include/LinkedList.h"
 
+/**
+ * @brief Sets the head of the left and right half of the list and separates the sub-lists. 
+ * 
+ * @param head the head of the original list
+ * @param leftHead the head of the left half of the list
+ * @param rightHead the head of the right half of the list
+ */
 void split(Node* head, Node** leftHead, Node** rightHead) {
-    Node* fast = head;
+    // Use a fast and slow pointer to find the middle of the original list
+    Node* fast = head->next;
     Node* slow = head;
-
-    while (fast->next != nullptr) {
+    while (fast != nullptr) {
+        // Fast iterates twice as fast as slow
         fast = fast->next;
-        if (fast->next != nullptr) {
+        if (fast != nullptr) {
             slow = slow->next;
             fast = fast->next;
         }
     }
 
+    // Set the left and right heads of the lists
     *leftHead = head;
     *rightHead = slow->next;
+    // Separate the left and right lists
     slow->next = nullptr;
 }
 
+/**
+ * @brief Merge the left and right lists in sorted order and returns the new head of the sorted list.
+ * 
+ * @param leftHead the left sub-list to merge
+ * @param rightHead the right sub-list to merge
+ * @return Node* the new head of the merged and sorted list
+ */
 Node* merge(Node* leftHead, Node* rightHead) {
-    // Return the left or right if the opposite split is empty
+    // Return the left or right if the opposite is empty
     if (leftHead == nullptr) {
         return rightHead;
     } else if (rightHead == nullptr) {
         return leftHead;
     }
 
+    // Cursor of left sub-list
     Node* leftCursor = leftHead;
+    // Cursor of right sub-list
     Node* rightCursor = rightHead;
+    // Cursor of the final merged list
     Node* cursor;
+
+    // First identify the head of the merged list
     if (*leftCursor <= *rightCursor) {
         cursor = leftCursor;
         leftCursor = leftCursor->next;
@@ -46,7 +70,9 @@ Node* merge(Node* leftHead, Node* rightHead) {
     }
     Node* newHead = cursor;
 
+    // Iterator left and right cursor until one can no longer iterate
     while (leftCursor != nullptr && rightCursor != nullptr) {
+        // Merge left/right cursor node so it is sorted and iterate left/right cursor
         if (*leftCursor <= *rightCursor) {
             cursor->next = leftCursor;
             leftCursor = leftCursor->next;
@@ -54,9 +80,12 @@ Node* merge(Node* leftHead, Node* rightHead) {
             cursor->next = rightCursor;
             rightCursor = rightCursor->next;
         }
+
+        // Iterate cursor of merged list
         cursor = cursor->next;
     }
 
+    // If left/right still has nodes left, merge the rest
     if (leftCursor == nullptr) {
         cursor->next = rightCursor;
     } else {
@@ -66,25 +95,36 @@ Node* merge(Node* leftHead, Node* rightHead) {
     return newHead;
 }
 
+/**
+ * @brief Performs recursive merge sort on the list until all nodes are merged. 
+ * 
+ * @param head the head of the list to merge sort on
+ * @return Node* the head of the sorted and merged list
+ */
 Node* mergeSort(Node* head) {
-    // Base case
+    // Base case if list is empty or contains one node
     if ((head == nullptr) || (head->next == nullptr)) {
         return head;
     }
 
-    // Split into sublists
+    // Split into left and right sub-lists
     Node* leftHead;
     Node* rightHead;
     split(head, &leftHead, &rightHead);
 
-    // Recursively split the linked list in half
+    // Recursively split the left and right sub-lists in half
     leftHead = mergeSort(leftHead);
     rightHead = mergeSort(rightHead);
 
-    // Merge the lists together
+    // Merge the left and right sub-lists in sorted order
     return merge(leftHead, rightHead);
 }
 
+/**
+ * @brief Runs the merge sort function to perform in-place merge sort.
+ * 
+ * @param list the list to sort
+ */
 void sort(LinkedList* list) {
     list->head = mergeSort(list->head);
 }
